0c0980bd90b4ba0ab2c92944db3ff86656434b01
Knightler <aryan.haghighinew@gmail.com>
2025-12-19 21:00:04 +0300

    started the folder on http practice before delving into building the reverse proxy in golang

--- main.go
+++ main.go
@@ -5,6 +5,10 @@ import (
 	"net/http"
 )
 
+func init() {
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.
+}
+
 func main() {
 	log.Println("Starting reverse proxy on :8080")
 	http.ListenAndServe(":8080", nil)
